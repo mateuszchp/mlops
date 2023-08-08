c0d87bb4c68d405721b9fa86fd9d53934b1f6e39
mateuszchp <92255993+mateuszchp@users.noreply.github.com>
2023-08-08 10:30:34 +0200

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -41,7 +41,7 @@ def split_data(df):
     X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure',
                'TricepsThickness', 'SerumInsulin', 'BMI', 'DiabetesPedigree',
                'Age']].values, df['Diabetic'].values
-    return train_test_split(X, y, test_size=0.30, random_state=0)
+    return train_test_split(X, y, test_size=0.29, random_state=0)
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
